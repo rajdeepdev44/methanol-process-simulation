9d2ffeb8b511017f082619b3b4dc1283c26188f9
Rajdeep Dev <rajdeepdev44@gmail.com>
2021-04-02 21:23:02 +0000

    initial comiit

---
+++ jekyll-theme-cs50.gemspec
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require File.expand_path("../lib/jekyll-theme-cs50/constants", __FILE__)
+
+Gem::Specification.new do |spec|
+
+  spec.authors = ["CS50"]
+  spec.files = `git ls-files -z`.split("\x0").select { |f| f.match(%r!^(_layouts|_includes|_sass|assets|lib|node_modules)/|LICENSE.txt$!i) }
+  spec.homepage = "https://cs50.harvard.edu/"
+  spec.license = "MIT"
+  spec.name = "jekyll-theme-cs50"
+  spec.summary = "This is CS50's theme for Jekyll."
+  spec.version = "1.1.0"
+
+  spec.add_runtime_dependency "deep_merge", "1.2.1"
+  spec.add_runtime_dependency "jekyll", "4.1.1"
+  spec.add_runtime_dependency "sanitize", "5.2.1"
+
+  CS50::PLUGINS.each do |gem, version|
+    spec.add_runtime_dependency gem, version
+  end
+
+end
---
+++ lib/jekyll-theme-cs50/constants.rb
@@ -0,0 +1,50 @@
+module CS50
+
+  PLUGINS = {
+    "jekyll-algolia" => "1.6.0",
+    "jekyll-default-layout" => "0.1.5",
+    "jekyll-optional-front-matter" => "0.3.2",
+    "jekyll-redirect-from" => "0.16.0",
+    "jekyll-titles-from-headings" => "0.5.3"
+  }.freeze
+
+  DEFAULTS = {
+    "cs50" => {
+      "locale" => "en",
+      "tz" => "America/New_York"
+    },
+    "exclude" => [
+      "Gemfile",
+      "Gemfile.lock",
+      "vendor"
+    ],
+    "include" => [
+      "license.md" # For OCW
+    ],
+    "optional_front_matter" => {
+      "remove_originals" => true
+    },
+    "plugins"  => CS50::PLUGINS.keys
+  }.freeze
+
+  OVERRIDES = {
+    "kramdown" => {
+      "gfm_quirks" => "paragraph_end",
+      "hard_wrap" => false,
+      "input" => "GFM",
+      "math_engine" => "mathjax",
+      "syntax_highlighter" => "rouge",
+      "template" => ""
+    },
+    "markdown" => "kramdown",
+    "permalink" => "pretty",
+    "redirect_from" => {
+      "json" => false
+    },
+    "sass" => {
+      "style" => "compressed"
+    },
+    "theme" => "jekyll-theme-cs50"
+  }.freeze
+
+end
